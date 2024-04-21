6e3e8db0dac8a4294ef96ab3f8d4a87194436468
Salah eddine <salaheddine.elhabouni@gmail.com>
2024-04-21 16:14:32 +0100

    creer un header et un drop down menu

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('utype')->default('USR')->comment('ADM for admin,and USR for normal user');
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,29 +46,106 @@
         
         <!--responsive.css-->
         <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
-        @livewireStyles
-        
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		
-        <!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
 
+
+        @livewireStyles
     </head>
 	
 	<body>
-		<!--[if lte IE 9]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-        <![endif]-->
-	
+		
+
+		<header>
+			<div class="default-header">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-3 col-md-2">
+							<div class="logo">
+								<a href="index.php"><img src="assets/images/logg.png" alt="image"/></a>
+							</div>
+						</div>
+						<div class="col-sm-9 col-md-10">
+							<div class="header_info">
+								<div class="header_widgets">
+									<div class="circle_icon"> <i class="fa fa-envelope" aria-hidden="true"></i> </div>
+									<p class="uppercase_text">For Support Mail us : </p>
+									<a href="mailto:info@example.com">codeprojectsorg@gmail.com</a>
+								</div>
+								<div class="header_widgets">
+									<div class="circle_icon"> <i class="fa fa-phone" aria-hidden="true"></i> </div>
+									<p class="uppercase_text">Service Helpline Call Us: </p>
+									<a href="tel:61-1234-5678-09">+91-9876543210</a>
+								</div>
+								<div class="social-follow">
+									<ul>
+										<li><a href="https://code-projects.org/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+										<li><a href="https://code-projects.org/"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+										<li><a href="https://code-projects.org/"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+										<li><a href="https://code-projects.org/"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+										<li><a href="https://code-projects.org/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+									</ul>
+								</div>
+								<div class="login_btn">
+									<a href="#loginform" class="btn btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		
+			<!-- Navigation -->
+			<nav id="navigation_bar" class="navbar navbar-default">
+				<div class="container">
+					<div class="navbar-header">
+						<button id="menu_slide" data-target="#navigation" aria-expanded="false" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
+							<span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
+						</button>
+					</div>
+					<div class="header_wrap">
+						<div class="user_login">
+							<ul>
+								<li class="dropdown"> <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle" aria-hidden="true"></i> Username <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+									<ul class="dropdown-menu">
+										<li><a href="profile.php">Profile Settings</a></li>
+										<li><a href="update-password.php">Update Password</a></li>
+										<li><a href="my-booking.php">My Booking</a></li>
+										<li><a href="post-testimonial.php">Post a Testimonial</a></li>
+										<li><a href="my-testimonials.php">My Testimonial</a></li>
+										<li><a href="logout.php">Sign Out</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+						<div class="header_search">
+							<div id="search_toggle"><i class="fa fa-search" aria-hidden="true"></i></div>
+							<form action="#" method="get" id="header-search-form">
+								<input type="text" placeholder="Search..." class="form-control">
+								<button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+							</form>
+						</div>
+					</div>
+					<div class="collapse navbar-collapse" id="navigation">
+						<ul class="nav navbar-nav">
+							<li><a href="index.php">Home</a></li>
+							<li><a href="page.php?type=aboutus">About Us</a></li>
+							<li><a href="car-listing.php">Car Listing</a></li>
+							<li><a href="page.php?type=faqs">FAQs</a></li>
+							<li><a href="contact-us.php">Contact Us</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<!-- Navigation end -->
+		
+		</header>
+		
+		
 		<!--welcome-hero start -->
 		<section id="home" class="welcome-hero">
+		{{-- top-area Start  --}}
 
-			<!-- top-area Start -->
-			<div class="top-area">
-				<!-- <div class="header-area">
+			{{-- <div class="top-area">
+				<div class="header-area">
 					<!-- Start Navigation -->
 				    <nav class="navbar navbar-default bootsnav  navbar-sticky navbar-scrollspy"  data-minus-value-desktop="70" data-minus-value-mobile="55" data-speed="1000">
 
@@ -94,7 +171,7 @@
 				                    <li class="scroll"><a href="#brand">brands</a></li>
 				                    <li class="scroll"><a href="#contact">contact</a></li>
 									{{-- <li class="scroll"><a href="#contact">My account</a></li> --}}
-                                    <li class="dropdown">
+                                    {{-- <li class="dropdown">
                                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">My Account <span class="caret"></span></a>
                                         <ul class="dropdown-menu">
                                             <li><a href="#">Profile</a></li>
@@ -110,9 +187,9 @@
 				</div><!--/.header-area -->
 			    <div class="clearfix"></div>
 
-			</div><!-- /.top-area-->
+			</div><!-- /.top-area--> --}} 
 			<!-- top-area End -->
-{{$slot}}
+			{{$slot}}
 		<!--contact start-->
 		<footer id="contact"  class="contact">
 			<div class="container">
