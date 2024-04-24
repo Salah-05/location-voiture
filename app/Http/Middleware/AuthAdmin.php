19cb945ddc1897b13b6bd0da26a774ae385aeb94
Salah eddine <salaheddine.elhabouni@gmail.com>
2024-04-24 16:42:04 +0100

    declarer le middleware de l'admin

--- app/Http/Middleware/AuthAdmin.php
+++ app/Http/Middleware/AuthAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use  Illuminate\Support\Facades\Auth;
 
 class AuthAdmin
 {
@@ -16,6 +17,13 @@ class AuthAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        if(Auth::user()->utype === 'ADM'){
+            return $next($request);
+        }
+        else{
+            session()->flush();
+            return redirect()-> route('login');
+        }
+       
     }
 }
