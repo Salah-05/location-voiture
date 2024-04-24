2f94f7f0333270a7cfb295ac7111d819d304da63
Salah eddine <salaheddine.elhabouni@gmail.com>
2024-04-24 16:40:09 +0100

    creer les vue et les classe (dashboard) pour les clients et l'admin

---
+++ resources/views/livewire/user/user-dashboard-component.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <h1>lowla</h1>
+    <h1>User Dashboard</h1>
+    <h1>Heloo liviwire why he doesn't work </h1>
+    <h1>User Dashboard</h1>
+    <h1>Heloo liviwire why he doesn't work </h1>
+    <h1>Heloo liviwire why he doesn't work </h1>
+</div> 
+
+
